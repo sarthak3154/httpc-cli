aabebf5bd854c3245e8956f88499a27f4311ed5b
sarthak3154 <sarthak.arora3154@gmail.com>
2018-10-27 13:45:49 -0400

    http get post request server handling init

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 require('./arguments');
 const net = require('net');
 
-handleClient = (socket) => {
-    socket.on('data', buf => {
+handleRequest = (buf) => {
+    const request = buf.toString('utf8');
+    const reqData = request.split('\r\n');
+    const typeHeader = reqData[0].toLowerCase();
+    if (typeHeader.includes(GET_CONSTANT)) {
+        //TODO GET Request handling
+    } else if (typeHeader.includes(POST_CONSTANT)) {
+        //TODO POST Request handling
+    } else {
+        //TODO INVALID Request handling
+    }
+};
 
+handleClient = (socket) => {
+    console.log(`New Client Connected from ${JSON.stringify(socket.address())}`);
+    socket.on('data', buf => {
+        handleRequest(buf);
     }).on('error', err => {
         console.log(`Socket error ${err}`);
         socket.destroy();
