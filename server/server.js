a301e8de0271f29ee003e3e93a86e09febb6dee6
sarthak3154 <sarthak.arora3154@gmail.com>
2018-10-29 19:37:52 -0400

    server yargs arg parsing and httpfs linking

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
-require('./arguments');
+#!/usr/bin/env node
+
+require('../arguments');
 const net = require('net');
+const yargs = require('yargs');
 
 handleRequest = (buf) => {
     const request = buf.toString('utf8');
@@ -26,12 +29,31 @@ handleClient = (socket) => {
     });
 };
 
+const argv = yargs.usage('httpfs is a simple file server.\n\nusage: httpfs [-v] [-p PORT] [-d PATH-TO-DIR]')
+    .default('port', 8080)
+    .option('v', {
+        default: false,
+        description: 'Prints debugging messages.'
+    })
+    .option('p', {
+        alias: 'port',
+        description: 'Specifies the port number that the server will listen and serve at.\n' +
+            'Default is 8080.'
+    })
+    .option('d', {
+        description: 'Specifies the directory that the server will use to read/write requested files. ' +
+            'Default is the current directory when launching the application.'
+    })
+    .help('help')
+    .argv;
+
+
 const server = net.createServer(handleClient)
     .on('error', err => {
         throw err;
     });
 
-server.listen({port: DEFAULT_PORT}, () => {
+server.listen({port: argv.port || DEFAULT_PORT}, () => {
     console.log('Server listening at port ' +
         (server.address().hasOwnProperty('port') ? server.address().port : server.address()));
 });
